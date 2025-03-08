7d1da52d1644d55b0be97b650e6c929b7b8560b9
Eder venancio <ederpvenancio@gmail.com>
2025-03-07 22:02:15 -0300

    Remoçao de validação em NFSeDTO

--- src/app/DTO/NFSeDTO.php
+++ src/app/DTO/NFSeDTO.php
@@ -4,8 +4,6 @@ namespace Sysborg\FocusNfe\app\DTO;
 
 use Carbon\Carbon;
 use Sysborg\FocusNFe\App\DTO\ServicoDTO;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 
 class NFSeDTO extends DTO {
     public function __construct(
@@ -23,20 +21,12 @@ class NFSeDTO extends DTO {
      */
     public static function fromArray(array $data): self
     {
+        $prestador = PrestadorDTO::fromArray($data['prestador']);
+        $tomador = TomadorDTO::fromArray($data['tomador']);
+        $servico = ServicoDTO::fromArray($data['servico']);
 
-        $validatedData = Validator::make($data, [
-            'data_emissao' => 'required|date',
-            'prestador' => 'required|array',
-            'tomador' => 'required|array',
-            'servico' => 'required|array'
-        ])->validate(); 
-    
-        $prestador = PrestadorDTO::fromArray($validatedData['prestador']);
-        $tomador = TomadorDTO::fromArray($validatedData['tomador']);
-        $servico = ServicoDTO::fromArray($validatedData['servico']);
-    
         return new self(
-            $validatedData['data_emissao'],
+            new Carbon($data['data_emissao']),
             $prestador,
             $tomador,
             $servico
