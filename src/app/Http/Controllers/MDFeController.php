8c6640eab092d11fa27a10bdf5b7273f9a8add3a
Eder venancio <ederpvenancio@gmail.com>
2025-03-09 21:35:43 -0300

    Ajuste em métodos e melhorias

--- src/app/Http/Controllers/MDFeController.php
+++ src/app/Http/Controllers/MDFeController.php
@@ -3,16 +3,19 @@
 namespace Sysborg\FocusNFe\app\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Http\Request;
-use Facades\Sysborg\FocusNFe\app\Services\MDFe;
+use Sysborg\FocusNfe\app\DTO\MDFeDTO;
+use Sysborg\FocusNFe\app\Services\MDFe;
+use Sysborg\FocusNFe\app\Http\Requests\MDFeRequest;
 
 class MDFeController extends Controller
 {
    
-    public function store(Request $request)
-    {
-        return response()->json(MDFe::envia($request->all()), 201);
-    }
+    public function store(MDFeRequest $request)
+{
+    $dto = MDFeDTO::fromArray($request->validated());
+    return response()->json(MDFe::create($dto), 201);
+}
+
 
     /**
      * Consulta um MDF-e pelo ID.
@@ -28,13 +31,13 @@ class MDFeController extends Controller
     }
 
   
-    public function incluiCondutor(Request $request, string $referencia)
+    public function incluiCondutor(MDFeRequest $request, string $referencia)
     {
         return response()->json(MDFe::incluiCondutor($referencia, $request->all()));
     }
 
    
-    public function incluiDFe(Request $request, string $referencia)
+    public function incluiDFe(MDFeRequest $request, string $referencia)
     {
         return response()->json(MDFe::incluiDFe($referencia, $request->all()));
     }
--- src/app/Http/Controllers/NFSeController.php
+++ src/app/Http/Controllers/NFSeController.php
@@ -6,7 +6,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Sysborg\FocusNFe\app\Http\Requests\NFSeRequest;
 use Sysborg\FocusNFe\app\DTO\NFSeDTO;
-use Facades\Sysborg\FocusNFe\app\Services\NFSe;
+use Sysborg\FocusNFe\app\Services\NFSe;
 
 class NFSeController extends Controller
 {
